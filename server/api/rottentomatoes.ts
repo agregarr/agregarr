7add64ef01e1faa17a73d31f4f40cfa493f159c6
grokdesigns <chris@grokdesigns.com>
2026-01-09 09:54:27 -0700

    Prettier

--- server/api/rottentomatoes.ts
+++ server/api/rottentomatoes.ts
@@ -84,21 +84,37 @@ class RottenTomatoes extends ExternalAPI {
    */
   private convertRomanToArabic(str: string): string {
     const romanMap: Record<string, number> = {
-      'I': 1, 'II': 2, 'III': 3, 'IV': 4, 'V': 5,
-      'VI': 6, 'VII': 7, 'VIII': 8, 'IX': 9, 'X': 10,
-      'XI': 11, 'XII': 12, 'XIII': 13, 'XIV': 14, 'XV': 15,
-      'XVI': 16, 'XVII': 17, 'XVIII': 18, 'XIX': 19, 'XX': 20
+      I: 1,
+      II: 2,
+      III: 3,
+      IV: 4,
+      V: 5,
+      VI: 6,
+      VII: 7,
+      VIII: 8,
+      IX: 9,
+      X: 10,
+      XI: 11,
+      XII: 12,
+      XIII: 13,
+      XIV: 14,
+      XV: 15,
+      XVI: 16,
+      XVII: 17,
+      XVIII: 18,
+      XIX: 19,
+      XX: 20,
     };
-    
+
     let result = str;
     // Match Roman numerals with word boundaries
     const romanRegex = /\b(X{0,2})(IX|IV|V?I{0,3})\b/gi;
-    
+
     result = result.replace(romanRegex, (match) => {
       const upper = match.toUpperCase();
       return romanMap[upper] !== undefined ? romanMap[upper].toString() : match;
     });
-    
+
     return result;
   }
 
@@ -107,12 +123,28 @@ class RottenTomatoes extends ExternalAPI {
    */
   private convertArabicToRoman(str: string): string {
     const arabicMap: [number, string][] = [
-      [20, 'XX'], [19, 'XIX'], [18, 'XVIII'], [17, 'XVII'], [16, 'XVI'],
-      [15, 'XV'], [14, 'XIV'], [13, 'XIII'], [12, 'XII'], [11, 'XI'],
-      [10, 'X'], [9, 'IX'], [8, 'VIII'], [7, 'VII'], [6, 'VI'],
-      [5, 'V'], [4, 'IV'], [3, 'III'], [2, 'II'], [1, 'I']
+      [20, 'XX'],
+      [19, 'XIX'],
+      [18, 'XVIII'],
+      [17, 'XVII'],
+      [16, 'XVI'],
+      [15, 'XV'],
+      [14, 'XIV'],
+      [13, 'XIII'],
+      [12, 'XII'],
+      [11, 'XI'],
+      [10, 'X'],
+      [9, 'IX'],
+      [8, 'VIII'],
+      [7, 'VII'],
+      [6, 'VI'],
+      [5, 'V'],
+      [4, 'IV'],
+      [3, 'III'],
+      [2, 'II'],
+      [1, 'I'],
     ];
-    
+
     let result = str;
     // Match 1-20 as standalone numbers or at end of string
     result = result.replace(/\b([1-9]|1[0-9]|20)\b/g, (match) => {
@@ -120,7 +152,7 @@ class RottenTomatoes extends ExternalAPI {
       const found = arabicMap.find(([n]) => n === num);
       return found ? found[1] : match;
     });
-    
+
     return result;
   }
 
@@ -200,22 +232,20 @@ class RottenTomatoes extends ExternalAPI {
       if (!movie) {
         const nameWithArabic = this.convertRomanToArabic(nameLower);
         const nameWithRoman = this.convertArabicToRoman(nameLower);
-        
-        movie = contentResults.hits.find(
-          (movie) => {
-            const titleLower = movie.title.toLowerCase();
-            const titleWithArabic = this.convertRomanToArabic(titleLower);
-            const titleWithRoman = this.convertArabicToRoman(titleLower);
-            
-            return (
-              Math.abs(movie.releaseYear - year) <= 1 &&
-              (titleWithArabic === nameWithArabic || 
-               titleWithRoman === nameWithRoman ||
-               titleLower === nameWithArabic ||
-               titleLower === nameWithRoman)
-            );
-          }
-        );
+
+        movie = contentResults.hits.find((movie) => {
+          const titleLower = movie.title.toLowerCase();
+          const titleWithArabic = this.convertRomanToArabic(titleLower);
+          const titleWithRoman = this.convertArabicToRoman(titleLower);
+
+          return (
+            Math.abs(movie.releaseYear - year) <= 1 &&
+            (titleWithArabic === nameWithArabic ||
+              titleWithRoman === nameWithRoman ||
+              titleLower === nameWithArabic ||
+              titleLower === nameWithRoman)
+          );
+        });
       }
 
       if (!movie) {
@@ -318,22 +348,20 @@ class RottenTomatoes extends ExternalAPI {
         if (!tvshow) {
           const nameWithArabic = this.convertRomanToArabic(nameLower);
           const nameWithRoman = this.convertArabicToRoman(nameLower);
-          
-          tvshow = contentResults.hits.find(
-            (series) => {
-              const titleLower = series.title.toLowerCase();
-              const titleWithArabic = this.convertRomanToArabic(titleLower);
-              const titleWithRoman = this.convertArabicToRoman(titleLower);
-              
-              return (
-                Math.abs(series.releaseYear - year) <= 1 &&
-                (titleWithArabic === nameWithArabic || 
-                 titleWithRoman === nameWithRoman ||
-                 titleLower === nameWithArabic ||
-                 titleLower === nameWithRoman)
-              );
-            }
-          );
+
+          tvshow = contentResults.hits.find((series) => {
+            const titleLower = series.title.toLowerCase();
+            const titleWithArabic = this.convertRomanToArabic(titleLower);
+            const titleWithRoman = this.convertArabicToRoman(titleLower);
+
+            return (
+              Math.abs(series.releaseYear - year) <= 1 &&
+              (titleWithArabic === nameWithArabic ||
+                titleWithRoman === nameWithRoman ||
+                titleLower === nameWithArabic ||
+                titleLower === nameWithRoman)
+            );
+          });
         }
       } else {
         // If no year provided, use exact case-insensitive title match
@@ -345,21 +373,19 @@ class RottenTomatoes extends ExternalAPI {
         if (!tvshow) {
           const nameWithArabic = this.convertRomanToArabic(nameLower);
           const nameWithRoman = this.convertArabicToRoman(nameLower);
-          
-          tvshow = contentResults.hits.find(
-            (series) => {
-              const titleLower = series.title.toLowerCase();
-              const titleWithArabic = this.convertRomanToArabic(titleLower);
-              const titleWithRoman = this.convertArabicToRoman(titleLower);
-              
-              return (
-                titleWithArabic === nameWithArabic || 
-                titleWithRoman === nameWithRoman ||
-                titleLower === nameWithArabic ||
-                titleLower === nameWithRoman
-              );
-            }
-          );
+
+          tvshow = contentResults.hits.find((series) => {
+            const titleLower = series.title.toLowerCase();
+            const titleWithArabic = this.convertRomanToArabic(titleLower);
+            const titleWithRoman = this.convertArabicToRoman(titleLower);
+
+            return (
+              titleWithArabic === nameWithArabic ||
+              titleWithRoman === nameWithRoman ||
+              titleLower === nameWithArabic ||
+              titleLower === nameWithRoman
+            );
+          });
         }
       }
 
